a431a580e1d38a8796353988626c728a8f830041
juancwu <46619361+juancwu@users.noreply.github.com>
2026-01-07 10:48:50 -0500

    update project list

--- data/data.go
+++ data/data.go
@@ -23,48 +23,14 @@ func GetPageData() Page {
 	return Page{
 		Projects: []Project{
 			{
-				Name:        "Hermes",
+				Name:        "Budgit",
 				URL:         "",
-				Description: "The user-friendly terminal API client for those who are in love and addicted to do everything on the terminal.",
-				Repo:        "https://github.com/juancwu/hermes",
-				Classes:     "border-orange-400",
+				Description: "Simple expense tracker with real-time grocery list.",
+				Repo:        "https://git.juancwu.dev/juancwu/budgit",
+				Classes:     "border-blue-300",
 				TechList: []Tech{
-					{TechName: "Rust", TechURL: "https://www.rust-lang.org/", TechClasses: ""},
-					{TechName: "Ratatui", TechURL: "https://ratatui.rs/", TechClasses: ""},
-				},
-			},
-			{
-				Name:        "Konbini",
-				URL:         "",
-				Description: "A \"convenient store\" with API services to manage projects' secrets or environment variables with the ability to separate the variables into their own environment.",
-				Repo:        "https://github.com/juancwu/konbini",
-				Classes:     "border-sky-500",
-				TechList: []Tech{
-					{TechName: "Golang", TechURL: "https://go.dev/", TechClasses: "transition bg-zinc-950 text-zinc-100"},
-					{TechName: "Echo", TechURL: "https://echo.labstack.com/", TechClasses: "bg-zinc-950 text-zinc-100"},
-					{TechName: "PostgreSQL", TechURL: "https://www.postgresql.org/", TechClasses: "bg-zinc-950 text-zinc-100"},
-				},
-			},
-			{
-				Name:        "EmojiScript",
-				URL:         "https://emojiscript.juancwu.dev/",
-				Description: "A silly project started for a brain rot hackathon host by Audrey Chen. Please give it a shot if you are interested!",
-				Repo:        "https://github.com/KonferCA/EmojiScript",
-				Classes:     "border-green-400",
-				TechList: []Tech{
-					{TechName: "Typescript", TechURL: "https://www.typescriptlang.org/", TechClasses: ""},
-					{TechName: "Devpost", TechURL: "https://brainrot-jia-seed-hackathon.devpost.com/", TechClasses: ""},
-				},
-			},
-			{
-				Name:        "Konfer Website",
-				URL:         "https://konfer.ca",
-				Description: "The official landing site for Konfer Inc.",
-				Repo:        "https://github.com/KonferCA/Konfer.ca",
-				Classes:     "border-indigo-300",
-				TechList: []Tech{
-					{TechName: "React", TechURL: "https://react.dev/", TechClasses: ""},
-					{TechName: "Firebase", TechURL: "https://firebase.google.com/", TechClasses: ""},
+					{TechName: "Go", TechURL: "https://go.dev/", TechClasses: ""},
+					{TechName: "PostgreSQL", TechURL: "https://postgresql.org", TechClasses: ""},
 					{TechName: "TailwindCSS", TechURL: "https://tailwindcss.com/", TechClasses: ""},
 				},
 			},
@@ -74,6 +40,18 @@ func GetPageData() Page {
 				Description: "HawkHacks is a 36 hour in-person hackathon hosted at Wilfrid Laurier University.",
 				Repo:        "https://github.com/LaurierHawkHacks/Landing",
 				Classes:     "border-orange-300",
+				TechList: []Tech{
+					{TechName: "React", TechURL: "https://react.dev/", TechClasses: ""},
+					{TechName: "Firebase", TechURL: "https://firebase.google.com/", TechClasses: ""},
+					{TechName: "TailwindCSS", TechURL: "https://tailwindcss.com/", TechClasses: ""},
+				},
+			},
+			{
+				Name:        "Konfer Website",
+				URL:         "https://konfer.ca",
+				Description: "The official landing site for Konfer Inc.",
+				Repo:        "https://github.com/KonferCA/Konfer.ca",
+				Classes:     "border-indigo-300",
 				TechList: []Tech{
 					{TechName: "React", TechURL: "https://react.dev/", TechClasses: ""},
 					{TechName: "Firebase", TechURL: "https://firebase.google.com/", TechClasses: ""},
@@ -102,16 +80,6 @@ func GetPageData() Page {
 					{TechName: "Next.js", TechURL: "https://nextjs.org/", TechClasses: "transition bg-zinc-950 text-zinc-100 hover:bg-zinc-100 hover:text-gray-950"},
 					{TechName: "Drizzle ORM", TechURL: "https://orm.drizzle.team/", TechClasses: "transition bg-zinc-950 text-zinc-100 hover:bg-green-600"},
 					{TechName: "Turso", TechURL: "https://turso.tech/", TechClasses: "transition bg-zinc-950 text-zinc-100 hover:bg-teal-600"},
-				},
-			},
-			{
-				Name:        "Hachi-Bitto",
-				URL:         "",
-				Description: "A general proramming language created just to satify YAPL, which stands for \"yet another programming language\".",
-				Repo:        "https://github.com/juancwu/hachi-bitto",
-				Classes:     "border-emerald-500",
-				TechList: []Tech{
-					{TechName: "Golang", TechURL: "https://go.dev/", TechClasses: "bg-zinc-950 text-zinc-100"},
 				},
 			},
 			{
